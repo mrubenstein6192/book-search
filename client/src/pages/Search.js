b5b77fe9d0f1bf32463ecf5bf7960eed9778f35f
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-10-20 18:20:30 -0400

    SEARCH DESIGN: - Added Jumbotron and search field

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 // import API from "../utils/API";
 // import { Link } from "react-router-dom";
+import { Container, Row, Col } from 'reactstrap';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
 
 class Search extends Component {
     state = {
@@ -25,82 +27,40 @@ class Search extends Component {
 
     render() {
         return (
-            <div>
-                <Form>
-                    <FormGroup>
-                        <Label for="exampleEmail">Email</Label>
-                        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="examplePassword">Password</Label>
-                        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleSelect">Select</Label>
-                        <Input type="select" name="select" id="exampleSelect">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </Input>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleSelectMulti">Select Multiple</Label>
-                        <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </Input>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleText">Text Area</Label>
-                        <Input type="textarea" name="text" id="exampleText" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleFile">File</Label>
-                        <Input type="file" name="file" id="exampleFile" />
-                        <FormText color="muted">
-                            This is some placeholder block-level help text for the above input.
-                            It's a bit lighter and easily wraps to a new line.
-        </FormText>
-                    </FormGroup>
-                    <FormGroup tag="fieldset">
-                        <legend>Radio Buttons</legend>
-                        <FormGroup check>
-                            <Label check>
-                                <Input type="radio" name="radio1" />{' '}
-                                Option one is this and that—be sure to include why it's great
-          </Label>
-                        </FormGroup>
-                        <FormGroup check>
-                            <Label check>
-                                <Input type="radio" name="radio1" />{' '}
-                                Option two can be something else and selecting it will deselect option one
-          </Label>
-                        </FormGroup>
-                        <FormGroup check disabled>
-                            <Label check>
-                                <Input type="radio" name="radio1" disabled />{' '}
-                                Option three is disabled
-          </Label>
-                        </FormGroup>
-                    </FormGroup>
-                    <FormGroup check>
-                        <Label check>
-                            <Input type="checkbox" />{' '}
-                            Check me out
-        </Label>
-                    </FormGroup>
-                    <Button>Submit</Button>
-                </Form>
+            <Container>
+                <Row>
+                    <Col>
+                        <Jumbotron fluid>
+                            <Container fluid>
+                                <center>
+                                    <h1 className="display-3">Book Lovers welcomed here.</h1>
+                                    <br></br>
+
+                                    <p className="lead">Ever wish you had your own personal butler for books? </p>
+
+                                    <p className="lead">With Bookmarker Pro you can search for your favorite books, and even save them for later.
+                                We take reading seriously.</p>
+                                </center>
+                            </Container>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+
+                <Row>
+
+                    <Col sm="12" md={{ size: 7, offset: 2 }}>
+                        <Form>
+                            <FormGroup>
+                                <Label for="userSearch">Search all the books.</Label>
+                                <Input type="input" name="search" id="bookSearch" placeholder="Search for a book. Ex. 'Harry Potter'" />
+                            </FormGroup>
+                            <Button>Submit</Button>
+                        </Form>
+                    </Col>
+                </Row>
 
 
-            </div>
-
-
+            </Container>
 
 
 
