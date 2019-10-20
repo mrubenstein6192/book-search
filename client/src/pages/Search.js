0bd8bc115665c3eb9ff84daf15f9fc64c6f75175
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-10-20 17:19:22 -0400

    Hopefully fixing Heroku by adding mongodb lab

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -12,69 +12,31 @@ class Search extends Component {
         author: "",
     };
 
-    // componentDidMount() {
-    //     this.loadBooks();
-    // }
+    componentDidMount() {
+        this.loadBooks();
+    }
 
-    // loadBooks = () => {
-    //     API.getBooks()
-    //         .then(res =>
-    //             this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-    //         )
-    //         .catch(err => console.log(err));
-    // };
+    loadBooks = () => {
+        API.getBooks()
+            .then(res =>
+                this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+            )
+            .catch(err => console.log(err));
+    };
 
 
-    // handleInputChange = event => {
-    //     const { name, value } = event.target;
-    //     this.setState({
-    //         [name]: value
-    //     });
-    // };
-
-    // handleFormSubmit = event => {
-    //     event.preventDefault();
-    //     if (this.state.title && this.state.author) {
-    //         API.saveBook({
-    //             title: this.state.title,
-    //             author: this.state.author,
-    //             synopsis: this.state.synopsis
-    //         })
-    //             .then(res => this.loadBooks())
-    //             .catch(err => console.log(err));
-    //     }
-    // };
 
     render() {
         return (
-
             <div>
 
 
-                <h1>What Books Do You Want to Search For?</h1>
-
-
-                {/* <Input
-                            value={this.state.title}
-                            onChange={this.handleInputChange}
-                            name="title"
-                            placeholder="Title"
-                        />
-                        <Input
-                            value={this.state.author}
-                            onChange={this.handleInputChange}
-                            name="author"
-                            placeholder="Author"
-                        />
-                        <FormBtn
-                            disabled={!(this.state.author && this.state.title)}
-                            onClick={this.handleFormSubmit}
-                        >
-                            Submit Book
-              </FormBtn> */}
-
 
             </div>
+
+
+
+
 
 
         );
