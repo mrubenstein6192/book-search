90eb9fd684e8bed8be738d55c654b82f0eb8473e
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-10-22 12:56:25 -0400

    quick typing fix

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -13,12 +13,12 @@ export default {
        
 
         console.log("Query search sent from front end form", query);
-        console.log("Our API Key maybe", process.env.REACT_APP_GOOGLE_BOOKS_API_KEY);
+        // console.log("Our API Key maybe", process.env.REACT_APP_GOOGLE_BOOKS_API_KEY);
 
         let thisSearch = "https://www.googleapis.com/books/v1/volumes?q=" + query + "+inauthor:keyes&key="
             + '"' + process.env.REACT_APP_GOOGLE_BOOKS_API_KEY + '"';
 
-        console.log("This is our search link", thisSearch);
+        // console.log("This is our search link", thisSearch);
 
         return axios.get(thisSearch);
 
